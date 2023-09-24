d7f2d9c7ac51c401a54516241b0108361b8fd34c
e-kondr01 <e.kondr01@gmail.com>
2023-09-24 11:51:19 +0300

    doc: add manager initialization params to example

--- examples/app/managers.py
+++ examples/app/managers.py
@@ -1,6 +1,8 @@
 from fastapi_sqlalchemy_toolkit import ModelManager
 
-from .models import Child
+from .models import Child, Parent
 from .schemas import CreateChildSchema, PatchChildSchema
 
-child_manager = ModelManager[Child, CreateChildSchema, PatchChildSchema](Child)
+child_manager = ModelManager[Child, CreateChildSchema, PatchChildSchema](
+    Child, default_ordering=Child.title, fk_mapping={"parent_id": Parent}
+)
