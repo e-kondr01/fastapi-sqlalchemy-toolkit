5d8c822fdee5da68b45a9545cd127f0d5a548784
negodinaarina <negodinaarina24@gmail.com>
2025-05-12 14:49:13 +0300

    feat: check if object exists during validate_unique_constraints() only if query is not empty

--- fastapi_sqlalchemy_toolkit/model_manager.py
+++ fastapi_sqlalchemy_toolkit/model_manager.py
@@ -1090,22 +1090,23 @@ class ModelManager(Generic[ModelT, CreateSchemaT, UpdateSchemaT]):
         """
         for unique_constraint in self.unique_constraints:
             query = {}
-            for field in unique_constraint:
-                if in_obj[field] is not None:
-                    query[field] = in_obj[field]
-            object_exists = await self.exists(
-                session, **query, where=(self.model.id != in_obj.get("id"))
-            )
-            if object_exists:
-                conflicting_fields = ", ".join(unique_constraint)
-                raise HTTPException(
-                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                    detail=(
-                        f"{self.model.__tablename__} с такими "
-                        + conflicting_fields
-                        + " уже существует."
-                    ),
+            if query:
+                for field in unique_constraint:
+                    if in_obj[field] is not None:
+                        query[field] = in_obj[field]
+                object_exists = await self.exists(
+                    session, **query, where=(self.model.id != in_obj.get("id"))
                 )
+                if object_exists:
+                    conflicting_fields = ", ".join(unique_constraint)
+                    raise HTTPException(
+                        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                        detail=(
+                            f"{self.model.__tablename__} с такими "
+                            + conflicting_fields
+                            + " уже существует."
+                        ),
+                    )
 
     async def validate_nullable_unique_constraints(
         self, session: AsyncSession, in_obj: ModelDict
